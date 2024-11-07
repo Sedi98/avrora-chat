141b43d3b860289996646d3b4adbc37b901f55fc
Sedi98 <sedi9816@gmail.com>
2024-11-08 01:43:35 +0400

    some changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = () => {
   return (
     <header className="bg-[#212121] text-white p-4 flex justify-between items-center">
-      <h1 className="text-lg font-semibold">Foody AI Chat</h1>
+      <h1 className="text-lg font-semibold">Avrora</h1>
       <div className="flex items-center space-x-4">
         <span className="text-sm">Salam, İstifadəçi!</span>
       </div>
